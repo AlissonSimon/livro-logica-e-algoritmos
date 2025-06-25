b9219b30dd31b05ea65dfbd3e9584b5e164b2a91
Alisson Simon <alissonsimon21@gmail.com>
2025-06-24 21:53:58 -0300

    Ajuste Exemplos - Capítulo 06

--- cap06/ex6_7/ex6_7.js
+++ cap06/ex6_7/ex6_7.js
@@ -1,9 +1,3 @@
-// c) Elaborar um programa que simule saques em um caixa eletrônico de um
-// banco. Ler o valor solicitado por clientes até ser digitado 0. Sabendo que o
-// caixa dispõe apenas de notas de 10, exiba após cada leitura se o saque é
-// válido ou inválido. Ao final, listar os saques válidos e a soma dos saques.
-//     Exiba também o número de saques inválidos.
-
 const prompt = require('prompt-sync')();
 
 const saques = [];
